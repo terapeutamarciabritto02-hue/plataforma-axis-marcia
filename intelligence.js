60f5275a96a72e1f10e30077fa7d3f43e9893e18
MARCIA BRITTO <terapeutamarciabritto02@gmail.com>
2026-05-18 18:33:14 -0400

    Criar intelligence.js

---
+++ intelligence.js
@@ -0,0 +1,9 @@
+✔ Diagnóstico energético
+✔ Análise vibracional
+✔ Chakra dominante
+✔ Frequência emocional
+✔ Sugestão automática de mesas
+✔ Escaneamento holográfico
+✔ IA simulada
+✔ Gráficos inteligentes
+✔ Alertas energéticos
