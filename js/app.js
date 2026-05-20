2117bc81bf872f89535ab99c687b0e668d1b48bd
MARCIA BRITTO <terapeutamarciabritto02@gmail.com>
2026-05-19 20:25:06 -0400

    Atualizar o app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
-   ├── app.js
-   ├── dashboard.js
-   ├── biometro.js
-   ├── axis-ai.js
-   ├── particles.js
-   └── login.js
+
+── app.js
+│   ├── biometro.js
+│   ├── chakras.js
+│   ├── intelligence.js
+│   ├── dashboard.js
+│   ├── sessions.js
+│   ├── symbols.js
+│   ├── audio.js
+│   └── database.js
+│.  ├── app.js
+    ├── dashboard.js
+    ├── biometro.js
+    ├── axis-ai.js
+    ├── particles.js
+    └── login.js
